5198dc247269ee478ec4abbfdd0fe07716460be4
Ali Marjaninejad <alimarjaninejad@Alis-MacBook-Pro.local>
2019-09-12 17:11:12 -0700

    exp2_2way is added now

---
+++ experiment_2_run_2way.py
@@ -0,0 +1,206 @@
+
+# next is to add accel and see the difference
+# add stiffness too
+import numpy as np
+from matplotlib import pyplot as plt
+from all_functions import *
+import pickle
+import os
+from warnings import simplefilter
+
+simplefilter(action='ignore', category=FutureWarning)
+
+experiment_ID = "experiment_2_2way"
+mc_run_number = 2
+babbling_time = 3
+number_of_refinements = 5
+errors_all_cyc_A_A = np.zeros([2, number_of_refinements+1, mc_run_number])
+errors_all_cyc_A_B = np.zeros([2, number_of_refinements+1, mc_run_number])
+errors_all_cyc_B_B = np.zeros([2, number_of_refinements+1, mc_run_number])
+errors_all_cyc_B_A = np.zeros([2, number_of_refinements+1, mc_run_number])
+
+errors_all_p2p_A_A = np.zeros([2, number_of_refinements+1, mc_run_number])
+errors_all_p2p_A_B = np.zeros([2, number_of_refinements+1, mc_run_number])
+errors_all_p2p_B_B = np.zeros([2, number_of_refinements+1, mc_run_number])
+errors_all_p2p_B_A = np.zeros([2, number_of_refinements+1, mc_run_number])
+
+stiffness_version_A = 5
+MuJoCo_model_name_A="nmi_leg_w_chassis_air_v{}.xml".format(stiffness_version_A)
+stiffness_version_B = 2
+MuJoCo_model_name_B="nmi_leg_w_chassis_air_v{}.xml".format(stiffness_version_B)
+
+random_seed = -1
+
+for mc_counter in range(mc_run_number):
+	random_seed+=1
+	# train model_A
+	np.random.seed(random_seed) # change the seed for different initial conditions
+	tf.random.set_random_seed(random_seed)
+	[babbling_kinematics, babbling_activations] =\
+		babbling_fcn(
+			MuJoCo_model_name=MuJoCo_model_name_A,
+			simulation_minutes=babbling_time,
+			kinematics_activations_show=False)
+	model_A_babble = inverse_mapping_fcn(
+		kinematics=babbling_kinematics,
+		activations=babbling_activations,
+		log_address="./logs/{}/scalars/babble_A_mc_run{}/".format(experiment_ID, mc_counter),
+		early_stopping=False)
+	cum_kinematics_A_babble = babbling_kinematics
+	cum_activations_A_babble = babbling_activations
+	#A_A test
+	np.random.seed(random_seed) # change the seed for different initial conditions
+	tf.random.set_random_seed(random_seed)
+	model_A_refined_A_cyc = copy_model_fcn(original_model=model_A_babble)
+	[model_A_refined_A_cyc, errors, cum_kinematics_A_refined, cum_activations_A_refined] =\
+		in_air_adaptation_fcn(
+			MuJoCo_model_name=MuJoCo_model_name_A,
+			model=model_A_refined_A_cyc,
+			babbling_kinematics=cum_kinematics_A_babble,
+			babbling_activations=cum_activations_A_babble,
+			number_of_refinements=number_of_refinements,
+			log_address="./logs/{}/scalars/refine_cyc_A_A_mc_run{}/".format(experiment_ID, mc_counter),
+			error_plots_show=False,
+			Mj_render=False)
+	errors_all_cyc_A_A[:,:, mc_counter] = [errors[0],errors[1]]
+
+	np.random.seed(random_seed) # change the seed for different initial conditions
+	tf.random.set_random_seed(random_seed)
+	model_A_refined_A_p2p = copy_model_fcn(original_model=model_A_babble)
+	[model_A_refined_A_p2p, errors, cum_kinematics_A_refined, cum_activations_A_refined] =\
+		p2p_run_fcn(
+			MuJoCo_model_name=MuJoCo_model_name_A,
+			model=model_A_refined_A_p2p,
+			babbling_kinematics=cum_kinematics_A_babble,
+			babbling_activations=cum_activations_A_babble,
+			number_of_refinements=number_of_refinements,
+			log_address="./logs/{}/scalars/refine_p2p_A_A_mc_run{}/".format(experiment_ID, mc_counter),
+			error_plots_show=False,
+			Mj_render=False)
+	errors_all_p2p_A_A[:,:, mc_counter] = [errors[0],errors[1]]
+	#A_B test
+	np.random.seed(random_seed) # change the seed for different initial conditions
+	tf.random.set_random_seed(random_seed)
+	model_A_refined_B_cyc = copy_model_fcn(original_model=model_A_babble)
+	[model_A_refined_B_cyc, errors, cum_kinematics_B_refined, cum_activations_B_refined] =\
+		in_air_adaptation_fcn(
+			MuJoCo_model_name=MuJoCo_model_name_B,
+			model=model_A_refined_B_cyc,
+			babbling_kinematics=cum_kinematics_A_babble,
+			babbling_activations=cum_activations_A_babble,
+			number_of_refinements=number_of_refinements,
+			log_address="./logs/{}/scalars/refine_cyc_A_B_mc_run{}/".format(experiment_ID, mc_counter),
+			error_plots_show=False,
+			Mj_render=False)
+	errors_all_cyc_A_B[:,:, mc_counter] = [errors[0],errors[1]]
+
+	np.random.seed(random_seed) # change the seed for different initial conditions
+	tf.random.set_random_seed(random_seed)
+	model_A_refined_B_p2p = copy_model_fcn(original_model=model_A_babble)
+	[model_A_refined_B_p2p, errors, cum_kinematics_B_refined, cum_activations_B_refined] =\
+		p2p_run_fcn(
+			MuJoCo_model_name=MuJoCo_model_name_B,
+			model=model_A_refined_B_p2p,
+			babbling_kinematics=cum_kinematics_A_babble,
+			babbling_activations=cum_activations_A_babble,
+			number_of_refinements=number_of_refinements,
+			log_address="./logs/{}/scalars/refine_p2p_A_B_mc_run{}/".format(experiment_ID, mc_counter),
+			error_plots_show=False,
+			Mj_render=False)
+	errors_all_p2p_A_B[:,:, mc_counter] = [errors[0],errors[1]]
+
+	# train model_B
+	np.random.seed(random_seed) # change the seed for different initial conditions
+	tf.random.set_random_seed(random_seed)
+	[babbling_kinematics, babbling_activations] =\
+		babbling_fcn(
+			MuJoCo_model_name=MuJoCo_model_name_B,
+			simulation_minutes=babbling_time,
+			kinematics_activations_show=False)
+	model_B_babble = inverse_mapping_fcn(
+		kinematics=babbling_kinematics,
+		activations=babbling_activations,
+		log_address="./logs/{}/scalars/babble_B_mc_run{}/".format(experiment_ID, mc_counter),
+		early_stopping=False)
+	cum_kinematics_B_babble = babbling_kinematics
+	cum_activations_B_babble = babbling_activations
+	#B_B test
+	np.random.seed(random_seed) # change the seed for different initial conditions
+	tf.random.set_random_seed(random_seed)
+	model_B_refined_B_cyc = copy_model_fcn(original_model=model_B_babble)
+	[model_B_refined_B_cyc, errors, cum_kinematics_B_refined, cum_activations_B_refined] =\
+		in_air_adaptation_fcn(
+			MuJoCo_model_name=MuJoCo_model_name_B,
+			model=model_B_refined_B_cyc,
+			babbling_kinematics=cum_kinematics_B_babble,
+			babbling_activations=cum_activations_B_babble,
+			number_of_refinements=number_of_refinements,
+			log_address="./logs/{}/scalars/refine_cyc_B_B_mc_run{}/".format(experiment_ID, mc_counter),
+			error_plots_show=False,
+			Mj_render=False)
+	errors_all_cyc_B_B[:,:, mc_counter] = [errors[0],errors[1]]
+
+	np.random.seed(random_seed) # change the seed for different initial conditions
+	tf.random.set_random_seed(random_seed)
+	model_B_refined_B_p2p = copy_model_fcn(original_model=model_B_babble)
+	[model_B_refined_B_p2p, errors, cum_kinematics_B_refined, cum_activations_B_refined] =\
+		p2p_run_fcn(
+			MuJoCo_model_name=MuJoCo_model_name_B,
+			model=model_B_refined_B_p2p,
+			babbling_kinematics=cum_kinematics_B_babble,
+			babbling_activations=cum_activations_B_babble,
+			number_of_refinements=number_of_refinements,
+			log_address="./logs/{}/scalars/refine_p2p_B_B_mc_run{}/".format(experiment_ID, mc_counter),
+			error_plots_show=False,
+			Mj_render=False)
+	errors_all_p2p_B_B[:,:, mc_counter] = [errors[0],errors[1]]
+
+	#B_A test
+	np.random.seed(random_seed) # change the seed for different initial conditions
+	tf.random.set_random_seed(random_seed)
+	model_B_refined_A_cyc = copy_model_fcn(original_model=model_B_babble)
+	[model_B_refined_A_cyc, errors, cum_kinematics_A_refined, cum_activations_A_refined] =\
+		in_air_adaptation_fcn(
+			MuJoCo_model_name=MuJoCo_model_name_A,
+			model=model_B_refined_A_cyc,
+			babbling_kinematics=cum_kinematics_B_babble,
+			babbling_activations=cum_activations_B_babble,
+			number_of_refinements=number_of_refinements,
+			log_address="./logs/{}/scalars/refine_cyc_B_A_mc_run{}/".format(experiment_ID, mc_counter),
+			error_plots_show=False,
+			Mj_render=False)
+	errors_all_cyc_B_A[:,:, mc_counter] = [errors[0],errors[1]]
+
+	np.random.seed(random_seed) # change the seed for different initial conditions
+	tf.random.set_random_seed(random_seed)
+	model_B_refined_B_p2p = copy_model_fcn(original_model=model_B_babble)
+	[model_B_refined_B_p2p, errors, cum_kinematics_B_refined, cum_activations_B_refined] =\
+		p2p_run_fcn(
+			MuJoCo_model_name=MuJoCo_model_name_A,
+			model=model_B_refined_A_cyc,
+			babbling_kinematics=cum_kinematics_B_babble,
+			babbling_activations=cum_activations_B_babble,
+			number_of_refinements=number_of_refinements,
+			log_address="./logs/{}/scalars/refine_p2p_B_A_mc_run{}/".format(experiment_ID, mc_counter),
+			error_plots_show=False,
+			Mj_render=False)
+	errors_all_p2p_B_A[:,:, mc_counter] = [errors[0],errors[1]]
+
+## saving the results
+os.makedirs("./results/{}".format(experiment_ID), exist_ok=True)
+np.save("./results/{}/errors_all_cyc_A_A".format(experiment_ID),errors_all_cyc_A_A)
+np.save("./results/{}/errors_all_cyc_A_B".format(experiment_ID),errors_all_cyc_A_B)
+np.save("./results/{}/errors_all_cyc_B_B".format(experiment_ID),errors_all_cyc_B_B)
+np.save("./results/{}/errors_all_cyc_B_A".format(experiment_ID),errors_all_cyc_B_A)
+
+np.save("./results/{}/errors_all_p2p_A_A".format(experiment_ID),errors_all_p2p_A_A)
+np.save("./results/{}/errors_all_p2p_A_B".format(experiment_ID),errors_all_p2p_A_B)
+np.save("./results/{}/errors_all_p2p_B_B".format(experiment_ID),errors_all_p2p_B_B)
+np.save("./results/{}/errors_all_p2p_B_A".format(experiment_ID),errors_all_p2p_B_A)
+
+## printing the results
+# print("errors_mean: ",errors_all_A_A.mean(1))
+# print("errors_std: ",errors_all_A_A.std(1))
+# print("errors_mean: ",errors_all_A_B.mean(1))
+# print("errors_std: ",errors_all_A_B.std(1))
+#import pdb; pdb.set_trace()
---
+++ experiment_2_show_2way_learningrates.py
@@ -0,0 +1,110 @@
+# next is to add accel and see the difference
+# add stiffness too
+import numpy as np
+from scipy import signal, stats
+from matplotlib import pyplot as plt
+from all_functions import *
+import pickle
+from warnings import simplefilter
+
+def exp2_learning_curves_cal_fcn(errors_all):
+	average_curve_mean = errors_all.mean(0).mean(1)
+	q0_curve_mean = errors_all[0].mean(1)
+	q1_curve_mean = errors_all[1].mean(1)
+	average_curve_std = errors_all.mean(0).std(1)
+	q0_curve_std = errors_all[0].std(1)
+	q1_curve_std = errors_all[1].std(1)
+	return average_curve_mean, q0_curve_mean, q1_curve_mean, average_curve_std, q0_curve_std, q1_curve_std 
+
+simplefilter(action='ignore', category=FutureWarning)
+
+import matplotlib
+matplotlib.rcParams['pdf.fonttype'] = 42
+matplotlib.rcParams['ps.fonttype'] = 42
+
+experiment_ID = "experiment_2_2way"
+number_of_refinements = 5
+errors_all_cyc_A_A = np.load("./results/{}/errors_all_cyc_A_A.npy".format(experiment_ID))
+errors_all_cyc_A_B = np.load("./results/{}/errors_all_cyc_A_B.npy".format(experiment_ID))
+errors_all_cyc_B_B = np.load("./results/{}/errors_all_cyc_B_B.npy".format(experiment_ID))
+errors_all_cyc_B_A = np.load("./results/{}/errors_all_cyc_B_A.npy".format(experiment_ID))
+
+errors_all_p2p_A_A = np.load("./results/{}/errors_all_p2p_A_A.npy".format(experiment_ID))
+errors_all_p2p_A_B = np.load("./results/{}/errors_all_p2p_A_B.npy".format(experiment_ID))
+errors_all_p2p_B_B = np.load("./results/{}/errors_all_p2p_B_B.npy".format(experiment_ID))
+errors_all_p2p_B_A = np.load("./results/{}/errors_all_p2p_B_A.npy".format(experiment_ID))
+
+number_of_mods = 8
+errors_all = np.zeros((number_of_mods,)+errors_all_cyc_A_A.shape)
+average_curve_mean_all = np.zeros([number_of_mods,number_of_refinements+1])
+q0_curve_mean_all = np.zeros([number_of_mods,number_of_refinements+1])
+q1_curve_mean_all= np.zeros([number_of_mods,number_of_refinements+1])
+average_curve_std_all = np.zeros([number_of_mods,number_of_refinements+1])
+q0_curve_std_all = np.zeros([number_of_mods,number_of_refinements+1])
+q1_curve_std_all= np.zeros([number_of_mods,number_of_refinements+1])
+errors_all = \
+np.array([errors_all_cyc_A_A,
+errors_all_cyc_A_B,
+errors_all_cyc_B_B,
+errors_all_cyc_B_A,
+errors_all_p2p_A_A,
+errors_all_p2p_A_B,
+errors_all_p2p_B_B,
+errors_all_p2p_B_A])
+for mod_iter in range(number_of_mods):
+	[average_curve_mean_all[mod_iter,:], q0_curve_mean_all[mod_iter,:], q1_curve_mean_all[mod_iter,:],
+	average_curve_std_all[mod_iter,:], q0_curve_std_all[mod_iter,:], q1_curve_std_all[mod_iter,:]] = \
+	exp2_learning_curves_cal_fcn(errors_all=errors_all[mod_iter,:])
+## plots
+show_p2p = False
+y_lim=[0, .85]
+
+if show_p2p:
+	nrows = 2
+else:
+	nrows = 1
+ncols = 3
+fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(9, 3))
+title_texts = ['average across both joints', 'proximal joint ($q_0$)', 'distal joint ($q_1$)']
+for mod_iter in range(number_of_mods):
+	#axes[np.divmod(mod_iter,3)[0]][np.divmod(mod_iter,3)[1]].plot(mean_curve_all[mod_iter,:])
+	if show_p2p:
+		if mod_iter < number_of_mods/2:
+			axes[0][0].errorbar(x=np.arange(number_of_refinements+1)+mod_iter/10, y=average_curve_mean_all[mod_iter,:], yerr=average_curve_std_all[mod_iter,:], capsize=2)
+			axes[0][1].errorbar(x=np.arange(number_of_refinements+1)+mod_iter/10, y=q0_curve_mean_all[mod_iter,:], yerr=q0_curve_std_all[mod_iter,:], capsize=2)
+			axes[0][2].errorbar(x=np.arange(number_of_refinements+1)+mod_iter/10, y=q1_curve_mean_all[mod_iter,:], yerr=q1_curve_std_all[mod_iter,:], capsize=2)
+			for ii in range(ncols):
+				plt.sca(axes[0][ii])
+				plt.xticks(range(number_of_mods), [])
+		else:
+			axes[1][0].errorbar(x=np.arange(number_of_refinements+1)+mod_iter/10, y=average_curve_mean_all[mod_iter,:], yerr=average_curve_std_all[mod_iter,:], capsize=2)
+			axes[1][1].errorbar(x=np.arange(number_of_refinements+1)+mod_iter/10, y=q0_curve_mean_all[mod_iter,:], yerr=q0_curve_std_all[mod_iter,:], capsize=2)
+			axes[1][2].errorbar(x=np.arange(number_of_refinements+1)+mod_iter/10, y=q1_curve_mean_all[mod_iter,:], yerr=q1_curve_std_all[mod_iter,:], capsize=2)
+			for ii in range(ncols):
+				plt.sca(axes[1][ii])
+				plt.xticks(range(number_of_mods), ['babbling', 'refinement #1','refinement #2','refinement #3','refinement #4','refinement #5'], rotation=30, ha='right')
+	else:
+		if mod_iter < number_of_mods/2:
+			axes[0].errorbar(x=np.arange(number_of_refinements+1)+mod_iter/10, y=average_curve_mean_all[mod_iter,:], yerr=average_curve_std_all[mod_iter,:], capsize=2)
+			axes[1].errorbar(x=np.arange(number_of_refinements+1)+mod_iter/10, y=q0_curve_mean_all[mod_iter,:], yerr=q0_curve_std_all[mod_iter,:], capsize=2)
+			axes[2].errorbar(x=np.arange(number_of_refinements+1)+mod_iter/10, y=q1_curve_mean_all[mod_iter,:], yerr=q1_curve_std_all[mod_iter,:], capsize=2)
+			for ii in range(ncols):
+				plt.sca(axes[ii])
+				plt.xticks(range(number_of_mods), ['babbling', 'refinement #1','refinement #2','refinement #3','refinement #4','refinement #5'], rotation=30, ha='right',fontsize=8)	
+				plt.title(title_texts[ii], fontsize=10)
+				if ii==0:
+					plt.ylabel("RMSE", fontsize=8)
+				else:
+					plt.yticks([])
+
+for subplot_iter in range(nrows*ncols):
+	if show_p2p:
+		axes[np.divmod(subplot_iter,3)[0]][np.divmod(subplot_iter,3)[1]].set_ylim(y_lim)
+		axes[np.divmod(subplot_iter,3)[0]][np.divmod(subplot_iter,3)[1]].legend(['A_A','A_B','B_B','B_A'], fontsize=6)
+	else:
+		axes[subplot_iter].set_ylim(y_lim)
+		axes[subplot_iter].legend(['A_A','A_B','B_B','B_A'], fontsize=6)
+fig.subplots_adjust(top=.9, bottom=.2, left=.06, right=.95)
+fig.savefig('./results/{}/learningcurves.png'.format(experiment_ID))
+plt.show()
+#import pdb; pdb.set_trace()
